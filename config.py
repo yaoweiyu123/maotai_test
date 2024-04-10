776389fec0cf5d89df3128e0865c0d4aab04b236
yaoweiyu123 <330417625@qq.com>
2024-04-10 10:52:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
+PUSH_TOKEN = 'de883efb1c7d4fd992b58c03b6508624'
 
 
 '''
